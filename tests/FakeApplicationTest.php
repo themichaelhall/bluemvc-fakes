6c2a6086c295b1ea0954cabb25d4f4de620ccad8
Michael Hall <hall@michaelhall.se>
2017-01-31 17:31:52 +0100

    Add tests for FakeApplication class

--- tests/FakeApplicationTest.php
+++ tests/FakeApplicationTest.php
@@ -1,9 +1,13 @@
 <?php
 
 use BlueMvc\Core\Exceptions\InvalidFilePathException;
+use BlueMvc\Core\Route;
 use BlueMvc\Fakes\FakeApplication;
 use DataTypes\Exceptions\FilePathInvalidArgumentException;
 use DataTypes\FilePath;
+
+require_once __DIR__ . '/Helpers/TestController.php';
+require_once __DIR__ . '/Helpers/TestViewRenderer.php';
 
 /**
  * Test FakeApplication class.
@@ -86,5 +90,108 @@ class FakeApplicationTest extends PHPUnit_Framework_TestCase
     public function testConstructorWithInvalidDocumentRootParameterType()
     {
         new FakeApplication(true);
+    }
+
+    /**
+     * Test getDocumentRoot method.
+     */
+    public function testGetDocumentRoot()
+    {
+        $DS = DIRECTORY_SEPARATOR;
+        $fakeApplication = new FakeApplication($DS . 'var' . $DS . 'www' . $DS);
+
+        $this->assertSame($DS . 'var' . $DS . 'www' . $DS, $fakeApplication->getDocumentRoot()->__toString());
+    }
+
+    /**
+     * Test getRoutes method.
+     */
+    public function testGetRoutes()
+    {
+        $fakeApplication = new FakeApplication();
+        $routes = $fakeApplication->getRoutes();
+
+        $this->assertSame(0, count($routes));
+    }
+
+    /**
+     * Test addRoute method.
+     */
+    public function testAddRoute()
+    {
+        $fakeApplication = new FakeApplication();
+        $fakeApplication->addRoute(new Route('', TestController::class));
+        $routes = $fakeApplication->getRoutes();
+
+        $this->assertSame(1, count($routes));
+    }
+
+    /**
+     * Test getViewPath method.
+     */
+    public function testGetViewPath()
+    {
+        $DS = DIRECTORY_SEPARATOR;
+        $fakeApplication = new FakeApplication($DS . 'var' . $DS . 'www' . $DS);
+
+        $this->assertSame($DS . 'var' . $DS . 'www' . $DS, $fakeApplication->getViewPath()->__toString());
+    }
+
+    /**
+     * Test setViewPath method.
+     */
+    public function testSetViewPath()
+    {
+        $DS = DIRECTORY_SEPARATOR;
+        $fakeApplication = new FakeApplication($DS . 'var' . $DS . 'www' . $DS);
+        $fakeApplication->setViewPath(FilePath::parse('views/'));
+
+        $this->assertSame($DS . 'var' . $DS . 'www' . $DS . 'views' . $DS, $fakeApplication->getViewPath()->__toString());
+    }
+
+    /**
+     * Test getTempPath method.
+     */
+    public function testGetTempPath()
+    {
+        $DS = DIRECTORY_SEPARATOR;
+        $fakeApplication = new FakeApplication();
+
+        $this->assertSame(sys_get_temp_dir() . $DS . 'bluemvc' . $DS . sha1($fakeApplication->getDocumentRoot()->__toString()) . $DS, $fakeApplication->getTempPath()->__toString());
+    }
+
+    /**
+     * Test setTempPath method.
+     */
+    public function testSetTempPath()
+    {
+        $DS = DIRECTORY_SEPARATOR;
+        $fakeApplication = new FakeApplication($DS . 'var' . $DS . 'www' . $DS);
+        $fakeApplication->setTempPath(FilePath::parse('temp/'));
+
+        $this->assertSame($DS . 'var' . $DS . 'www' . $DS . 'temp' . $DS, $fakeApplication->getTempPath()->__toString());
+    }
+
+    /**
+     * Test getViewRenderers method.
+     */
+    public function testGetViewRenderers()
+    {
+        $fakeApplication = new FakeApplication();
+
+        $this->assertSame(0, count($fakeApplication->getViewRenderers()));
+    }
+
+    /**
+     * Test addViewRenderer method.
+     */
+    public function testAddViewRenderer()
+    {
+        $fakeApplication = new FakeApplication();
+        $fakeApplication->addViewRenderer(new TestViewRenderer());
+        $viewRenderers = $fakeApplication->getViewRenderers();
+
+        $this->assertSame(1, count($viewRenderers));
+        $this->assertInstanceOf(TestViewRenderer::class, $viewRenderers[0]);
     }
 }
