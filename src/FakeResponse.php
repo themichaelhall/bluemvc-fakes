ee881387bc590fd932b33159bfced156addde3ef
Michael Hall <hall@michaelhall.se>
2017-09-12 22:38:23 +0200

    Add constructor to FakeResponse class

--- src/FakeResponse.php
+++ src/FakeResponse.php
@@ -8,6 +8,7 @@
 namespace BlueMvc\Fakes;
 
 use BlueMvc\Core\Base\AbstractResponse;
+use BlueMvc\Core\Interfaces\RequestInterface;
 
 /**
  * BlueMvc fake response class.
@@ -16,6 +17,18 @@ use BlueMvc\Core\Base\AbstractResponse;
  */
 class FakeResponse extends AbstractResponse
 {
+    /**
+     * Constructs the fake response.
+     *
+     * @since 1.0.0
+     *
+     * @param RequestInterface $request The request.
+     */
+    public function __construct(RequestInterface $request)
+    {
+        parent::__construct($request);
+    }
+
     /**
      * Does nothing.
      *
